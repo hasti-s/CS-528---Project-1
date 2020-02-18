f9d31f053533add1501b79a74d65d850c59e7ee5
Hasti <hastisharifi75@gmail.com>
2020-02-17 20:03:04 -0600

    commit 2

---
+++ Assets/registerationVoice.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class registerationVoice : MonoBehaviour
+{ 
+
+    public AudioSource audio;
+    public bool isPlaying;// = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        isPlaying = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (closeEnough())
+        {
+            if (!isPlaying)
+            {
+                isPlaying = true;
+                if (!audio.isPlaying)
+                {
+
+                    audio.Play();
+                }
+            }
+        }
+        else
+        {
+
+            isPlaying = false;
+            audio.Stop();
+        }
+
+
+    }
+
+    public bool closeEnough() {
+        if (Vector3.Distance(GameObject.Find("CAVE2-PlayerController").GetComponent<Transform>().position, transform.position) < 4)
+            return true;
+        else
+            return false;
+    }
+}
